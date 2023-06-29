89c4d9de6975d783fd2f1bb340f7e95b36e5b9a3
tolgabakir <tolga.bakir@bookreen.com>
2023-06-30 00:59:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Toolbar from './toolbar.tsx';
+
+function App() {
+
+
+
+  return (
+    <>
+    <Toolbar />
+    </> 
+  )
+}
+
+export default App
---
+++ src/toolbar.tsx
@@ -0,0 +1,57 @@
+import { useState, useRef, MouseEvent, useEffect } from "react";
+
+function Toolbar() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const dragRef = useRef<HTMLDivElement>(null);
+  const dragStartCoords = useRef({ x: 0, y: 0 });
+  const dragOffset = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      setIsDragging(false);
+    };
+
+    const handleMouseMove = (e: any) => {
+      if (!isDragging) return;
+
+      const deltaX = e.clientX - dragStartCoords.current.x;
+      const deltaY = e.clientY - dragStartCoords.current.y;
+
+      dragOffset.current = { x: deltaX, y: deltaY };
+      setPosition((prevPosition) => ({
+        x: prevPosition.x + deltaX,
+        y: prevPosition.y + deltaY,
+      }));
+
+      dragStartCoords.current = { x: e.clientX, y: e.clientY };
+    };
+
+    window.addEventListener("mouseup", handleMouseUp);
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp);
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isDragging]);
+
+  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    dragStartCoords.current = { x: e.clientX, y: e.clientY };
+    dragOffset.current = { x: 0, y: 0 };
+  };
+
+  return (
+    <div
+      className="toolbar"
+      style={{ position: "absolute", top: position.y, left: position.x }}
+      ref={dragRef}
+      onMouseDown={handleMouseDown}
+    >
+      {/* Your toolbar content */}
+    </div>
+  );
+}
+
+export default Toolbar;
